5d467667bb0746550d0d78860196d5ca40b57fe5
zhugechina <zhugetd@126.com>
2025-05-31 16:06:33 +0800

    add game

--- components/games/GamePage.tsx
+++ components/games/GamePage.tsx
@@ -67,7 +67,7 @@ const GamePage = ({
                     <div id="container-31a50d07a2355afffb47dc172427319e"></div>
                 </div> */}
                 {/* <div id="container-31a50d07a2355afffb47dc172427319e" className="w-full lg:w-[20%] min-h-[600px] bg-[#212233] mx-2"></div> */}
-                <div className="w-full lg:w-[20%] min-h-[600px] bg-[#212233] mx-2"></div>
+                <div className="w-full lg:w-[20%] bg-[#212233] mx-2"></div>
 
 
             </div>
@@ -75,12 +75,12 @@ const GamePage = ({
 
             <div id="article_and_recommand" className="w-full flex flex-col lg:flex-row justify-center items-start gap-10 mt-10">
 
-                <div id="article" className="w-full lg:w-[48%] flex flex-col justify-start items-center gap-4 border-4 border-green-900 rounded-2xl p-2">
+                <div id="article" className="order-2 md:order-1 w-full lg:w-[48%] flex flex-col justify-start items-center gap-4 border-4 border-green-900 rounded-2xl p-2">
                     <p className="text-2xl font-header font-semibold text-sky-500">About {main_game.name}</p>
                     {Article && <Article />}
                 </div>
 
-                <div id="recommand" className="w-full lg:w-[48%] flex flex-col py-2 justify-start items-center gap-2 border-4 border-green-900 rounded-2xl">
+                <div id="recommand" className="order-1 md:order-2 w-full lg:w-[48%] flex flex-col py-2 justify-start items-center gap-2 border-4 border-green-900 rounded-2xl">
                     <p className="text-2xl font-header font-semibold text-sky-500">Recommended Games</p>
                     <div className="w-full flex flex-wrap justify-center items-center gap-2 px-0 py-2">
                         {relate_games && relate_games.length > 10 && relate_games.slice(10).map(game => (
